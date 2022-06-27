1f3c77cf152765402641bb02bf8d05f6fc7b2214
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-06-27 16:23:06 +0300

    counter, local storage, inc button, reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const localStorageKeys = {
+        countValue: "countValue"
+    }
+
+    let startValue = 0
+    let endValue = 5
+    const [countValue, setCountValue] = useState<number>(startValue)
+
+    useEffect(() => {
+        const countValueFromLS = localStorage.getItem(localStorageKeys.countValue)
+        countValueFromLS && setCountValue(JSON.parse(countValueFromLS))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(localStorageKeys.countValue, JSON.stringify(countValue))
+    }, [countValue])
+
+    const incHandler = () => {
+        setCountValue(countValue + 1)
+    }
+
+    const resetHandler = () => {
+        setCountValue(startValue)
+        localStorage.setItem(localStorageKeys.countValue, JSON.stringify(startValue))
+    }
+
+
+    return (
+        <div className="App">
+
+            <h1>{countValue}</h1>
+            <button disabled={countValue === endValue} onClick={incHandler}>inc</button>
+            <button disabled={countValue === startValue} onClick={resetHandler}>reset</button>
+        </div>
+    );
 }
 
 export default App;
